c95d77503ba2bdf7cae4d61711fed61f9bf13a28
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-11-08 17:58:11 +0530

    CRUD Operations Implemented

---
+++ WebAPI/Controllers/CRUD-OperationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("CRUD Operations")]
+    public class CRUD_OperationController : ControllerBase
+    {
+        /*public IActionResult Index()
+        {
+            return Ok();
+        }*/
+
+        [HttpGet("Fetch Employee Details")]
+        public Employee FetchEmployeeDetails(Employee employee)
+        {
+            employee.employeeCurrentProject = "";
+            return employee;
+        }
+
+        [HttpPost("Add Employee Details")]
+        public Employee AddEmployeeDetails(Employee employee)
+        {
+            employee.employeeName = "Aryan";
+            employee.employeeDoJ = "October 3rd, 2022";
+            employee.employeeId = 1400;
+            employee.employeeCurrentProject = "None";
+
+            return employee;
+        }
+
+        [HttpPut("Update Employee Details")]
+        public Employee UpdateEmployeeDetails(Employee employee)
+        {
+            employee.employeeName = "Aryan Gupta";
+
+            return employee;
+        }
+
+        [HttpDelete("Remove Employee Details")]
+        public Employee RemoveEmployeeDetails(Employee employee)
+        {
+            employee.employeeName = "Aryan";
+            employee.employeeDoJ = "October 3rd, 2022";
+            employee.employeeId = 1400;
+
+            return employee;
+        }
+    }
+}
---
+++ WebAPI/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI
+{
+    public class Employee
+    {
+        public string employeeName { get; set; }
+
+        public int employeeId { get; set; }
+
+        public string employeeDoJ { get; set; }
+
+        public string employeeCurrentProject { get; set; }
+
+    }
+}
